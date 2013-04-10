c39f967897d9db307445e8722b71a00a99637c62
Shi Shaoguang <shishaoguang@blingstorm.com.cn>
2013-04-10 11:34:28 +0800

    修改魅族手机无法启动游戏问题,可能是shaders代码的问题,替换了部分猎鱼版本的shaders代码

--- cocos2dx/shaders/ccShader_PositionColorLengthTexture_frag.h
+++ cocos2dx/shaders/ccShader_PositionColorLengthTexture_frag.h
@@ -19,23 +19,36 @@
  * SOFTWARE.
  */
 
-"																															\n\
-#ifdef GL_ES																												\n\
-#extension GL_OES_standard_derivatives : enable																				\n\
-																															\n\
-varying mediump vec4 v_color;																								\n\
-varying mediump vec2 v_texcoord;																							\n\
-#else																														\n\
-varying vec4 v_color;																										\n\
-varying vec2 v_texcoord;																									\n\
-#endif																														\n\
-																															\n\
-void main()																													\n\
-{																															\n\
-#if defined GL_OES_standard_derivatives																						\n\
-	gl_FragColor = v_color*smoothstep(0.0, length(fwidth(v_texcoord)), 1.0 - length(v_texcoord));							\n\
-#else																														\n\
-	gl_FragColor = v_color*step(0.0, 1.0 - length(v_texcoord));																\n\
-#endif																														\n\
-}																															\n\
+//"																															\n\
+//#ifdef GL_ES																												\n\
+//#extension GL_OES_standard_derivatives : enable																				\n\
+//																															\n\
+//varying mediump vec4 v_color;																								\n\
+//varying mediump vec2 v_texcoord;																							\n\
+//#else																														\n\
+//varying vec4 v_color;																										\n\
+//varying vec2 v_texcoord;																									\n\
+//#endif																														\n\
+//																															\n\
+//void main()																													\n\
+//{																															\n\
+//#if defined GL_OES_standard_derivatives																						\n\
+//	gl_FragColor = v_color*smoothstep(0.0, length(fwidth(v_texcoord)), 1.0 - length(v_texcoord));							\n\
+//#else																														\n\
+//	gl_FragColor = v_color*step(0.0, 1.0 - length(v_texcoord));																\n\
+//#endif																														\n\
+//}																															\n\
+//";
+"                                                    \n\
+#ifdef GL_ES                                        \n\
+precision lowp float;                                \n\
+#endif                                                \n\
+\n\
+varying vec4 v_fragmentColor;                        \n\
+\n\
+void main()                                            \n\
+{                                                    \n\
+gl_FragColor = v_fragmentColor;                    \n\
+}                                                    \n\
 ";
+

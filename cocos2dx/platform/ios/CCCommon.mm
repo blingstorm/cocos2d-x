0ea25bda1a1a24b452aebed9dadc0664a7a62d3b
silwings <silwings@gmail.com>
2013-05-11 15:18:14 +0800

    remove text alignment

--- cocos2dx/platform/ios/CCCommon.mm
+++ cocos2dx/platform/ios/CCCommon.mm
@@ -55,11 +55,6 @@ void CCMessageBox(const char * pszMsg, const char * pszTitle)
                                                 cancelButtonTitle: @"OK"
                                                 otherButtonTitles: nil];
     [messageBox autorelease];
-    for (UIView *view in messageBox.subviews) {
-        if([[view class] isSubclassOfClass:[UILabel class]]) {
-            ((UILabel*)view).textAlignment = UITextAlignmentLeft;
-        }
-    }
     [messageBox show];
 }
 

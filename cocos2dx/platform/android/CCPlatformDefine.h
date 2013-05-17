20b13a26e687eb8904d2f9d20dc9a17bc8457030
ZhangZiqi <zhangziqi@blingstorm.com.cn>
2013-05-17 19:19:17 +0800

    在定义COCOS2D_DEBUG的情况下不弹出assert对话框

--- cocos2dx/platform/android/CCPlatformDefine.h
+++ cocos2dx/platform/android/CCPlatformDefine.h
@@ -3,14 +3,26 @@
 
 #define CC_DLL 
 
+#ifdef COCOS2D_DEBUG
 #define CC_ASSERT(cond, msg) \
 if (! (cond)) \
 { \
-    char content[256]; \
-    sprintf(content, "%s function:%s line:%d", __FILE__, __FUNCTION__, __LINE__);  \
-    CCMessageBox(content, "Assert error"); \
-    CCLog("Assert error:%s : %s",content, msg);\
+char content[256]; \
+sprintf(content, "%s function:%s line:%d", __FILE__, __FUNCTION__, __LINE__);  \
+CCMessageBox(content, "Assert error"); \
+CCLog("Assert error:%s : %s",content, msg);\
 }
+#else
+#define CC_ASSERT(cond, msg) \
+if (! (cond)) \
+{ \
+char content[256]; \
+sprintf(content, "%s function:%s line:%d", __FILE__, __FUNCTION__, __LINE__);  \
+CCLog("Assert error:%s : %s",content, msg);\
+}
+#endif
+
+
 
 
 #define CC_UNUSED_PARAM(unusedparam) (void)unusedparam

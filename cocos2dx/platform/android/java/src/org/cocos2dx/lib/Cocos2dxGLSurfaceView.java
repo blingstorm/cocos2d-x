36a0ad95e6275f529fc77b13ea67595df0e1619c
ZhangZiqi <zhangziqi@blingstorm.com.cn>
2013-05-18 11:08:27 +0800

    修复屏幕显示一半的问题

--- cocos2dx/platform/android/java/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
+++ cocos2dx/platform/android/java/src/org/cocos2dx/lib/Cocos2dxGLSurfaceView.java
@@ -282,7 +282,12 @@ public class Cocos2dxGLSurfaceView extends GLSurfaceView {
 	@Override
 	protected void onSizeChanged(final int pNewSurfaceWidth, final int pNewSurfaceHeight, final int pOldSurfaceWidth, final int pOldSurfaceHeight) {
 		if(!this.isInEditMode()) {
-			this.mCocos2dxRenderer.setScreenWidthAndHeight(pNewSurfaceWidth, pNewSurfaceHeight);
+			if(pNewSurfaceWidth > pNewSurfaceHeight) {
+				this.mCocos2dxRenderer.setScreenWidthAndHeight(pNewSurfaceWidth, pNewSurfaceHeight);
+			} else {
+				this.mCocos2dxRenderer.setScreenWidthAndHeight(pNewSurfaceHeight, pNewSurfaceWidth);
+			}
+			
 		}
 	}
 
